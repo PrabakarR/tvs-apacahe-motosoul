da45b4d37fcf0dd4e9f69f6ee14e90b57a2788dd
PrabakarR <prabajo06@gmail.com>
2023-03-08 12:58:14 +0530

    first commit

---
+++ pages/components/header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/Link'
+import logo from '../../../public/tvs-logo.png'
+const Header = () => {
+  return (
+   <header className='header-wrap'>
+     <div className='container'>
+        <div className="headerstriplit">
+           <Link href="#">
+            <Image alt='Logo' className='img-fluid' src={logo} width={130} height={22}></Image>
+           </Link>
+        </div>
+     </div>
+   </header>
+  )
+}
+
+export default Header
